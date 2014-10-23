3888730f11c74a33545682eb3168504a3974f5a8
pimsturm <pim.sturm@gmail.com>
2014-10-23 23:02:38 +0200

    Splashscreen

--- MutiboClient2/app/src/main/java/mutiboclient/moviesets/org/mutibo/Movies.java
+++ MutiboClient2/app/src/main/java/mutiboclient/moviesets/org/mutibo/Movies.java
@@ -25,7 +25,7 @@ public class Movies extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.movie_listview);
 
 
         movieList.add("Movie 1");
